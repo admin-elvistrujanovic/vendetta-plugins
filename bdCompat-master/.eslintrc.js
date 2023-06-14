a6c7285d7a86965ff7e2d3eb674c18b8d2f400f2
admin-elvistrujanovic <admin-elvistrujanovic@users.noreply.github.com>
2023-06-14 14:20:16 +0000

    deploy: 221f0637a6adfa8fde57b7ee33cb719cc92e60bd

--- bdCompat-master/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: ['eslint:recommended', 'vendetta'],
-  env: { node: true },
-};
--- bdCompat-master/modules/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-fs.readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
-  .forEach(filename => {
-    const moduleName = path.parse(filename).name;
-    exports[moduleName] = require(path.join(__dirname, filename));
-  });
